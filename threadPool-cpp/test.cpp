f1925c965f55ff81f5d6588428d395fdb533c68f
kokomiQU <143464561+kokomiQU@users.noreply.github.com>
2023-11-22 16:52:17 +0800

    Add files via upload

---
+++ threadPool-cpp/test.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "ThreadPool.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+void taskFunc(void* arg)
+{
+    int num = *(int*)arg;
+    printf("thread %ld is working ,number =%d\n", pthread_self(), num);
+    sleep(1);
+}
+
+int main()
+{
+    //创建线程池
+    ThreadPool pool(3, 10);
+    for (int i = 0; i < 100; ++i)
+    {
+        int* num = new int(i+100);
+        pool.addTask(Task(taskFunc,num));
+    }
+
+    sleep(20);
+
+
+    return 0;
+}
